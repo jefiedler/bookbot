e75b3473b6d8508f0f6acb2224e0111c8e974598
jefiedler <jefiedler@gmx.net>
2024-12-18 23:07:00 +0100

    refactor loading file content and add counting words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    book_path ="books/frankenstein.txt"
+    book_content = get_book_content(book_path)
+    book_words = get_book_words(book_content)
+
+    print(f"{book_words} words found in the document.")
+
+def get_book_content(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def get_book_words(file_content):
+    words = file_content.split()
+    return len(words)
+
+main()
