dc108bbe6a3c6da7c71fec6ba447ce2c056e856c
jefiedler <jefiedler@gmx.net>
2024-12-19 22:47:28 +0100

    add char count

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path ="books/frankenstein.txt"
     book_content = get_book_content(book_path)
     book_words = get_book_words(book_content)
-
-    print(f"{book_words} words found in the document.")
+    chars_dict = get_chars_dic(book_content)
+    
 
 def get_book_content(file_path):
     with open(file_path) as f:
@@ -12,5 +12,15 @@ def get_book_content(file_path):
 def get_book_words(file_content):
     words = file_content.split()
     return len(words)
+
+def get_chars_dic(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
