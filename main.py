5e442d05ad3c5c77e9012df95ecfd41d1f28b544
jefiedler <jefiedler@gmx.net>
2024-12-20 18:55:26 +0100

    add report printing function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     book_words = get_book_words(book_content)
     chars_dict = get_chars_dic(book_content)
     
+    print_report(book_path, book_words, chars_dict)
 
 def get_book_content(file_path):
     with open(file_path) as f:
@@ -23,4 +24,22 @@ def get_chars_dic(text):
             chars[lowered] = 1
     return chars
 
+def sort_word_count_text(dict):
+    return dict["count"]    
+
+
+def print_report(book_path, words_count, chars_dict): 
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words_count} words found in the document.\n")
+
+    word_count_text = []
+    for c in chars_dict:
+        if (c.isalpha()):
+            word_count_text.append({'count': chars_dict[c], 'text': f"The '{c}' character was found {chars_dict[c]} times"})
+
+    word_count_text.sort(reverse=True, key=sort_word_count_text)
+    for dic in word_count_text:
+        print(dic['text'])
+
+    print("--- End report ---")
 main()
